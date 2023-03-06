f7e29d7c6d4d795e24e070d4f89e13616d6c0435
Alexxx20 <aleksanderlucena@gmail.com>
2023-03-06 11:54:55 -0300

    Mais um

--- Downloader.py
+++ Downloader.py
@@ -1,5 +1,6 @@
 try:
     from pytube import YouTube
+    #from moviepy.editor import VideoFileClip, AudioFileClip
 except ModuleNotFoundError:
         import subprocess
         result = subprocess.run(['pip', '--version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -8,12 +9,21 @@ except ModuleNotFoundError:
 
         else:
             result = subprocess.run(['pip', 'install', 'pytube'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            result2 = subprocess.run(['pip', 'install', 'moviepy'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
             if result.returncode == 0:
                 print("A biblioteca pytube foi instalada com sucesso!")
             else:
                 print("Ocorreu um erro ao instalar a biblioteca pytube, tente instalá-la manualmente")
                 print(result.stderr.decode('utf-8'))
+
+            #if result2.returncode == 0:
+            #    print("A biblioteca moviepy foi instalada com sucesso!")
+            #else:
+            #    print("Ocorreu um erro ao instalar a biblioteca moviepy, tente instalá-la manualmente")
+            #    print(result2.stderr.decode('utf-8'))
         print("Reinicie o programa")
+
 import os
 
 links = input('Insira os links: ').split()
@@ -45,28 +55,38 @@ if resposta.lower() in ["yes", 'sim', 's']:
 
         
 else:
-    for i in links:
-        yt = YouTube(i)
+    try:
+        for i in links:
+            yt = YouTube(i)
 
-        print('Título = ', yt.title)
+            print('Título = ', yt.title)
 
-        resolucoes = yt.streams.filter(progressive=True)
-        for i in resolucoes:
-            print(i)
+            resolucoes = yt.streams.filter(progressive=True)
+            cont = 1
+            for i in resolucoes:
+                # if hasattr(i, 'res'):
+                #     print(f"{cont}. itag: {i.itag}, res: {i.res}")
+                # else:
+                #     print(f"{cont}. itag: {i.itag}, resolução não disponível")
+                print(i)
+                cont += 1
 
-        x = int(input("Digite a 'itag' das configurações desejadas "))
+            x = int(input("Digite a 'itag' das configurações desejadas "))
 
-        ys = yt.streams.get_by_itag(x)
-        
-        print('Baixando...')
+            ys = yt.streams.get_by_itag(x)
+            
+            print('Baixando...')
 
-        download_dir = os.path.join(os.path.expanduser("~"), "Videos", "Videos baixados")
+            download_dir = os.path.join(os.path.expanduser("~"), "Videos", "Videos baixados")
 
-        if not os.path.exists(download_dir):
-            os.makedirs(download_dir)
+            if not os.path.exists(download_dir):
+                os.makedirs(download_dir)
 
-        ys.download(download_dir)
+            ys.download(download_dir)
 
-    print('Downloads concluídos!')
-    print('\nAperte "Enter" para finalizar o programa')
-    input = input()
+        print('Downloads concluídos!')
+        print('\nAperte "Enter" para finalizar o programa')
+        input = input()
+    except Exception as e:
+        print(e)
+        input = input()
